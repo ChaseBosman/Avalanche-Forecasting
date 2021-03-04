472bf6d6996f3f79c11bc7ce07fd2d54cf6be2ed
ChaseBosman <54570221+ChaseBosman@users.noreply.github.com>
2021-03-03 22:33:18 -0800

    Implemented help command and improved instructions

--- Avalanche Forecasting/driver.cpp
+++ Avalanche Forecasting/driver.cpp
@@ -6,28 +6,31 @@
 int main() 
 {
 	std::map<std::string, std::unique_ptr<Response>> response_map;
-	bool complete = false;
 
-	while (!complete)
+	while (true)
 	{
-		cout << "Enter a Utah Avalanche Center location to search! Type 'exit' to leave the program.";
+		cout << "Enter a Utah Avalanche Center location to search. Type 'help' for help, 'exit' to leave the program: ";
 		std::string user_input;
 		cin >> user_input;
 		if (user_input == "exit") 
+			return EXIT_SUCCESS;
+		if(user_input == "help")
 		{
-			complete = true;
-			break;
+			cout << "Locations include: Logan, Ogden, Uintas, Salt-Lake, Provo, Skyline, Moab, and Abajos.\n";
 		}
-		// if the response is already cached in memory
-		if (response_map.find(user_input) == response_map.end())
+		// if the response isn't already cached
+		else if (response_map.find(user_input) == response_map.end())
 		{
 			// build the target path with the user input and save it to target
 			TargetBuilder curr__target(user_input);
 			std::string target = curr__target.get_target();
+
 			// handle the get requested target
 			RequestHandler handler(target);
+
 			// map the response of the user entered location into a map containing response objects
 			response_map.insert(std::make_pair(user_input, new Response(handler.get_response_body(), user_input)));
+
 			// print response
 			cout << response_map[user_input]->get_response() << '\n';
 		}
@@ -36,6 +39,7 @@ int main()
 			// location has already been requested and mapped
 			cout << response_map[user_input]->get_response() << '\n';
 		}
+		cout << '\n';
 	}
 	
 	return 0;
